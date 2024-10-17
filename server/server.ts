ce7adf84d682b1973c0ca1a4b695bd49f90cbc9f
jejolare <jejolare@gmail.com>
2024-10-17 18:26:45 +0700

    block fix

--- server/server.ts
+++ server/server.ts
@@ -647,12 +647,8 @@ const requestsLimiter = rateLimit({
                             }
 
                             const blockInfo = await Block.findOne({
-                                where: { tx_id: data.result?.tx_info?.keeper_block?.toString() },
+                                where: { height: data.result?.tx_info?.keeper_block?.toString() },
                             });
-
-
-                            console.log(blockInfo, data.result?.tx_info?.keeper_block?.toString());
-                            
 
                             res.json({
                                 ...data.result.tx_info, 
