b600e681d678f1761ecc2832ae9f66fa4a23145c
ravaga <trueravaga@gmail.com>
2025-01-23 12:59:46 +0400

    updated title
    Signed-off-by: ravaga <trueravaga@gmail.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,49 +1,77 @@
-import { StoreProvider } from '@/store/store-reducer';
-import './index.scss';
-import { AppProps, default as NextApp } from 'next/app';
-import Head from 'next/head';
-import NetMode from '@/interfaces/common/NetMode';
-import Layout from './Layout';
+import { StoreProvider } from "@/store/store-reducer";
+import "./index.scss";
+import { AppProps, default as NextApp } from "next/app";
+import Head from "next/head";
+import NetMode from "@/interfaces/common/NetMode";
+import Layout from "./Layout";
 
 interface AppCustomProps extends AppProps {
-    netMode: NetMode;
+  netMode: NetMode;
 }
 
 function App(data: AppCustomProps) {
-    const { Component, pageProps, netMode } = data;
-    return (
-        <>
-            <Head>
-                <title>Block explorer (Zarcanum)</title>
-                <meta name="description" content="Zano is an open-source cryptocurrency and ecosystem with enterprise-grade privacy, security, and scalability" />
-                <meta name="viewport" content="width=device-width, initial-scale=1  user-scalable=no" />
-                <link rel="icon" href="/favicon.ico" />
+  const { Component, pageProps, netMode } = data;
+  return (
+    <>
+      <Head>
+        <title>Zano Block explorer</title>
+        <meta
+          name="description"
+          content="Zano is an open-source cryptocurrency and ecosystem with enterprise-grade privacy, security, and scalability"
+        />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1  user-scalable=no"
+        />
+        <link rel="icon" href="/favicon.ico" />
 
-                <meta property="og:type" content="website"/>
-                <meta property="og:url" content="https://explorer.zano.org/"/>
-                <meta property="og:title" content="Block explorer (Zarcanum)"/>
-                <meta property="og:description" content="Zano is an open-source cryptocurrency and ecosystem with enterprise-grade privacy, security, and scalability"/>
-                <meta property="og:image" content={netMode === "MAIN" ? "social-banner.png" : "social-banner-testnet.png"}/>
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://explorer.zano.org/" />
+        <meta property="og:title" content="Zano Block explorer" />
+        <meta
+          property="og:description"
+          content="Zano is an open-source cryptocurrency and ecosystem with enterprise-grade privacy, security, and scalability"
+        />
+        <meta
+          property="og:image"
+          content={
+            netMode === "MAIN"
+              ? "social-banner.png"
+              : "social-banner-testnet.png"
+          }
+        />
 
-                <meta property="twitter:card" content="summary_large_image"/>
-                <meta property="twitter:url" content="https://explorer.zano.org/"/>
-                <meta property="twitter:title" content="Block explorer (Zarcanum)"/>
-                <meta property="twitter:description" content="Zano is an open-source cryptocurrency and ecosystem with enterprise-grade privacy, security, and scalability"/>
-                <meta property="twitter:image" content={netMode === "MAIN" ? "social-banner.png" : "social-banner-testnet.png"}/>
-            </Head>
-            <StoreProvider initial={{ netMode: netMode === "TEST" ? "TEST" : "MAIN" }}>
-                <Layout>
-                    <Component {...pageProps} />
-                </Layout>
-            </StoreProvider>
-        </>
-    );
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://explorer.zano.org/" />
+        <meta property="twitter:title" content="Zano Block explorer" />
+        <meta
+          property="twitter:description"
+          content="Zano is an open-source cryptocurrency and ecosystem with enterprise-grade privacy, security, and scalability"
+        />
+        <meta
+          property="twitter:image"
+          content={
+            netMode === "MAIN"
+              ? "social-banner.png"
+              : "social-banner-testnet.png"
+          }
+        />
+      </Head>
+      <StoreProvider
+        initial={{ netMode: netMode === "TEST" ? "TEST" : "MAIN" }}
+      >
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </StoreProvider>
+    </>
+  );
 }
 
 App.getInitialProps = async () => {
-    return {
-        netMode: process.env.NET_MODE === "MAIN" ? "MAIN" : "TEST" as NetMode,
-    }
+  return {
+    netMode: process.env.NET_MODE === "MAIN" ? "MAIN" : ("TEST" as NetMode),
+  };
 };
 
 export default App;
