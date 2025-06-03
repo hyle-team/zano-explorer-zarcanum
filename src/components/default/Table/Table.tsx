e4ad2c4066b13fe33fd372c4d6cdefb743efe038
AzizbekFayziyev <fayziyev.dev@gmail.com>
2025-06-03 18:46:48 +0500

    feature: add status texts

--- src/components/default/Table/Table.tsx
+++ src/components/default/Table/Table.tsx
@@ -5,7 +5,7 @@ import Input from "../../UI/Input/Input";
 import TableProps from "./Table.props";
 import Preloader from "@/components/UI/Preloader/Preloader";
 
-function Table(props: TableProps) { 
+function Table(props: TableProps) {
     function onNumberInput(event: React.FormEvent<HTMLInputElement>, setState?: React.Dispatch<React.SetStateAction<string>>, max?: number) {
         if (!setState) return;
         const newValue = event.currentTarget.value;
@@ -17,15 +17,16 @@ function Table(props: TableProps) {
         setState(newValue);
     }
 
-    const { 
-        headers, 
-        elements, 
-        pagination, 
-        className, 
-        hidePaginationBlock, 
-        itemsOnPage, 
-        setItemsOnPage, 
-        page, 
+    const {
+        headers,
+        headerStatus,
+        elements,
+        pagination,
+        className,
+        hidePaginationBlock,
+        itemsOnPage,
+        setItemsOnPage,
+        page,
         setPage,
         goToBlock,
         setGoToBlock,
@@ -46,87 +47,90 @@ function Table(props: TableProps) {
         <div className={className}>
             {!isLoading ? <>
                 <table className={`${styles.table} ${!textWrap ? styles["table__text_nowrap"] : ""}`}>
-                <thead>
-                    <tr>
-                        {
-                            headers.map(e => <th key={nanoid(16)}>{e}</th>)
-                        }
-                    </tr>
-                </thead>
-                <tbody>
-                    {elements.map(row => 
-                        <tr key={nanoid(16)}>
+                    <thead>
+                        <tr>
                             {
-                                row.map((e, i) => 
-                                    <td 
-                                        style={columnsWidth ? { width: `${columnsWidth[i]}%` } : undefined} 
-                                        key={nanoid(16)}
-                                    >
-                                        {e}
-                                    </td>
-                                )
+                                headers.map(e => <th key={nanoid(16)}>{e}</th>)
                             }
                         </tr>
-                    )}
-                    
-                </tbody>
-            </table>
-                <Pagination pagination={pagination}/>
+                        {headerStatus && <tr className={styles.table__info_status}>
+                            <th colSpan={headers.length}>{headerStatus}</th>
+                        </tr>}
+                    </thead>
+                    <tbody>
+                        {elements.map(row =>
+                            <tr key={nanoid(16)}>
+                                {
+                                    row.map((e, i) =>
+                                        <td
+                                            style={columnsWidth ? { width: `${columnsWidth[i]}%` } : undefined}
+                                            key={nanoid(16)}
+                                        >
+                                            {e}
+                                        </td>
+                                    )
+                                }
+                            </tr>
+                        )}
+
+                    </tbody>
+                </table>
+                <Pagination pagination={pagination} />
             </> : <div className={styles.preloader}>
-                <Preloader/>
-                </div>}
-                
+                <Preloader />
+            </div>}
+
         </div>
     )
 
-    function Pagination ({pagination}:{pagination:boolean | undefined}) {
+    function Pagination({ pagination }: { pagination: boolean | undefined }) {
         if (!pagination) return null
-        return   <div className={styles["table__pagination"]}>
-        <div className={styles["table__pagination__pages"]}>
-            <p>Pages: </p>
-            <div>
-                <button 
-                    className={page === "1" ? styles.disabled : undefined}
-                    onClick={() => changePage(-1)}
-                >
-                    <ArrowImg />
-                </button>
-                <button
-                    onClick={() => changePage(1)}
-                >
-                    <ArrowImg />
-                </button>
-            </div>
-            <Input 
-                type="text"
-                value={page}
-                onInput={(e) => onNumberInput(e, setPage)}
-            />
-        </div>
-        {pagesTotal && <p>Pages total: {pagesTotal}</p>}
-        <div className={styles["table__pagination__blocks"]}>
-            <div>
-                <p>Items on page: </p>
-                <Input 
-                    type="text" 
-                    value={itemsOnPage} 
-                    onInput={(e) => onNumberInput(e, setItemsOnPage, 50)} 
+        return <div className={styles["table__pagination"]}>
+            <div className={styles["table__pagination__pages"]}>
+                <p>Pages: </p>
+                <div>
+                    <button
+                        className={page === "1" ? styles.disabled : undefined}
+                        onClick={() => changePage(-1)}
+                    >
+                        <ArrowImg />
+                    </button>
+                    <button
+                        onClick={() => changePage(1)}
+                    >
+                        <ArrowImg />
+                    </button>
+                </div>
+                <Input
+                    type="text"
+                    value={page}
+                    onInput={(e) => onNumberInput(e, setPage)}
                 />
             </div>
-            {!hidePaginationBlock &&
+            {pagesTotal && <p>Pages total: {pagesTotal}</p>}
+            <div className={styles["table__pagination__blocks"]}>
                 <div>
-                    <p>Go to block: </p>
+                    <p>Items on page: </p>
                     <Input
-                        type="text" 
-                        placeholder="number" 
-                        value={goToBlock} 
-                        onInput={(e) => onNumberInput(e, setGoToBlock)} 
-                        onEnterPress={goToBlockEnter}
+                        type="text"
+                        value={itemsOnPage}
+                        onInput={(e) => onNumberInput(e, setItemsOnPage, 50)}
                     />
                 </div>
-            }
+                {!hidePaginationBlock &&
+                    <div>
+                        <p>Go to block: </p>
+                        <Input
+                            type="text"
+                            placeholder="number"
+                            value={goToBlock}
+                            onInput={(e) => onNumberInput(e, setGoToBlock)}
+                            onEnterPress={goToBlockEnter}
+                        />
+                    </div>
+                }
+            </div>
         </div>
-    </div>
     }
 }
 
